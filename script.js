65ca86b60afea72a4961e9ccfbde77b2a5bb3da1
iMac G1 <imacg1@iMac-de-Energy-21.local>
2022-05-06 14:15:06 +0000

    css

---
+++ script.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    let Name = document.querySelector("#Name");
+    let nom = document.querySelector("#nom");
+    nom.innerHTML =  Name.value;
+
+    let Email = document.querySelector("#Email");
+    let email = document.querySelector("#email");
+    email.innerHTML = Email.value;
+
+    let Names = document.querySelector("#names");
+    let names = document.querySelector("#nombre");
+    names.innerHTML = Names.value;
+
+    let Select = document.querySelector("#Select");
+    let select = document.querySelector("#selection");
+    select.innerHTML = Select.value;
+
+    let Experience = document.querySelector("#Experience");
+    let experience = document.querySelector("#experer");
+    experience.innerHTML = Experience.value;
+}       
